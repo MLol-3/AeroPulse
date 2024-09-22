79b42bd2b8360225950357a527e0d3ecde54ce68
Chonakan Chumtap <cshuhand@gmail.com>
2024-09-22 21:02:52 +0700

    :memo: : added visualization.

--- platform/backend/main.py
+++ platform/backend/main.py
@@ -1,3 +1,4 @@
+from sklearn.inspection import permutation_importance
 import gspread
 from google.oauth2.service_account import Credentials
 from typing import Union
@@ -8,10 +9,11 @@ import pickle
 
 def transform_pipeline(df):
     df = df.drop(["id"], axis=1)
+    print(df)
     encoder = pickle.load(open("../../pipelines/encoder_business.pkl", 'rb'))
     # encoder = joblib.load('../../pipelines/encoder_business.pkl')
-    scaler = joblib.load('../../pipelines/scaler.pkl')
-    model = joblib.load("../../pipelines/model_KNN_business.pkl")
+    model = pickle.load(open("../../pipelines/model_KNN_business.pkl", 'rb'))
+    scaler = pickle.load(open("../../pipelines/scaler.pkl", 'rb'))
     
     all_encoded = ['Inflight wifi service', 
                     'Ease of Online booking', 
@@ -25,19 +27,36 @@ def transform_pipeline(df):
 
     df_encoded = pd.DataFrame(encoded.toarray(), 
                             columns=encoder.get_feature_names_out(all_encoded))
-    # encoding
+    # # encoding
     df_encoded = df_encoded.astype(int)
-    df_encoded.to_csv("test.csv", index=False)
+    print(df_encoded.shape)
     # print(len(list(df_encoded)))
     # df_encoded = df_encoded[6:]
-    # df_drop_dummy = df[6:].drop(all_encoded, axis=1)
-    # df_final = pd.concat([df_encoded, df_drop_dummy], axis=1)
-    # # standardization
-    # df_scaler = scaler.transform(df_final)
-    # preds = model.predict(df_scaler)
-    # print(preds)
+    df_drop_dummy = df[6:].drop(all_encoded, axis=1)
     
-    # return preds
+    df_drop_dummy = df_drop_dummy.drop(["Gender", "Gate location", "Arrival Delay in Minutes", "Departure/Arrival time Convenient"], axis=1)
+    print(df_drop_dummy.shape)
+    df_drop_dummy_reset = df_drop_dummy.reset_index(drop=True)
+    df_encoded_reset = df_encoded.reset_index(drop=True)
+
+    df_final = pd.concat([df_encoded_reset, df_drop_dummy_reset], axis=1)
+    # print(df_final.iloc[:-6]["Departure/Arrival time Convenient"])
+    df_final.iloc[:-6].to_csv("hi.csv", index=False)
+    print(df_final.iloc[:-6].shape)
+    # # # standardization
+    df_scaler = scaler.transform(df_final.iloc[:-6])
+    preds = model.predict(df_scaler)
+    
+    # Calculate permutation importance on the test set
+    result = permutation_importance(model, df_scaler, y_test, n_repeats=10, random_state=42)
+
+    # Create a dataframe with feature importance scores
+    feature_importance = pd.DataFrame({
+        'Feature': df_drop_dummy.columns,
+        'Importance': result.importances_mean
+    }).sort_values(by='Importance', ascending=False)
+    
+    return preds
 
 
 app = FastAPI()
@@ -94,9 +113,6 @@ def get_summary():
     
     df = pd.DataFrame(data)
     preds = transform_pipeline(df)
-    
-    print(df)
-    
 
     # Return the data as JSON response
-    return {"data": df}
+    return {"data": data[6:], "preds": preds.tolist()}
--- platform/frontend/visualizer.py
+++ platform/frontend/visualizer.py
@@ -15,32 +15,59 @@ if __name__ == "__main__":
     st.set_page_config(layout='wide', page_title="AeroPulse Visualization", page_icon="✈️")
     
     # Create a container
-with st.container():
-    # Create two columns
-    col1, col2 = st.columns([6, 0.7])  # Adjust the width ratio to create space between the columns
-    with col1:
-        st.title("AeroPulse ✈️")
-        st.markdown("**- Classify Satisfaction, Neutral or Dissatisfaction of passenger based on history data -**")
-    with col2:
-        st.write("")
-        if st.button("Summarize The Flight 🧑🏻‍✈️", type="primary"):
-            res = getRawData()
-        else:
-            res = None
-    
-    st.markdown(
-        """
-        <style>
-        .stTabs [role="tablist"] {
-            margin-top: 30px; /* Adjust the value to change the margin */
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
+    with st.container():
+        # Create two columns
+        col1, col2 = st.columns([6, 1])  # Adjust the width ratio to create space between the columns
+        with col1:
+            st.title("AeroPulse ✈️")
+            st.markdown("**- Classify Satisfaction, Neutral or Dissatisfaction of passenger based on history data -**")
+        with col2:
+            st.write("")
+            if st.button("Summarize The Flight 🧑🏻‍✈️", type="primary"):
+                res = getRawData()
+            else:
+                res = None
+        
+        st.markdown(
+            """
+            <style>
+            .stTabs [role="tablist"] {
+                margin-top: 30px; /* Adjust the value to change the margin */
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
     )
-    
-    tab1, tab2 = st.tabs(["Raw Data", "Summary of Raw Data"])
 
+    with st.container(border=True):
+        st.markdown("<h3 style='margin-top: 20px; margin-bottom: 70px; text-align: center;'>US Airplane Flight 123</h3>", unsafe_allow_html=True)
+        col1, col2, col3 = st.columns(3)
+        
+        with col1:
+            if res != None:
+                st.image("./assets/AirplaneSeat.png")
+        with col2:
+            with st.container():
+                if res != None:
+                    df = pd.DataFrame(res.json()['data'])
+                    preds = res.json()['preds']
+                    
+                    df_preds = pd.DataFrame(preds)
+                    satisfaction_counts = df_preds.value_counts()
+                    count_satisfied = satisfaction_counts['satisfied']
+                    count_neutral_dissatisfied = satisfaction_counts['neutral or dissatisfied']
+                    
+                    col1, col2 = st.columns(2)
+                    
+                    col1.metric(label="Satisfied", value=f"{count_satisfied} 👤", delta="+ 😁")
+                    col2.metric(label="Neutral or Dissatisfied", value=f"{count_neutral_dissatisfied} 👤", delta="- 🫤")
+        with col3:
+            pass
+        
+        st.markdown("<h3 style='margin-bottom: 20px; text-align: center;'></h3>", unsafe_allow_html=True)
+    
+    tab1, tab2 = st.tabs(["View Raw Data", "Manual Classify"])
+    
     with tab1:
         with st.container():
             if res != None:
@@ -48,38 +75,5 @@ with st.container():
                 st.dataframe(df) 
             else:
                 st.markdown("Waiting for data...")
-    # with tab2:
-    #     with st.container():
-    #         if res != None:
-    #             df = pd.DataFrame(res.json()['data'])
-    #             # st.dataframe(df) 
-    #             # Example DataFrame
-    #             # Get the count of each unique value in 'ColumnA'
-    #             value_counts = df['satisfaction'].value_counts()
-    #             # Count the specific value 'A'
-    #             count_satisfied = value_counts['satisfied']
-    #             count_neutral_dissatisfied = value_counts['neutral or dissatisfied']
-                
-    #             col1, col2 = st.columns(2)
-                
-    #             col1.metric(label="Satisfied", value=f"{count_satisfied} people", delta="+ 😁")
-    #             col2.metric(label="Neutral or Dissatisfied", value=f"{count_neutral_dissatisfied} people", delta="- 🫤")
-    #         else:
-    #             st.markdown("Waiting for data...")
-                
-    
-    # # with st.container():
-    # #     col1, col2, col3 = st.columns(3)
-
-    # #     with col1:
-    # #         st.header("A cat")
-    # #         st.image("https://static.streamlit.io/examples/cat.jpg")
-
-    # #     with col2:
-    # #         st.header("A dog")
-    # #         st.image("https://static.streamlit.io/examples/dog.jpg")
-        
-    # #     with col3:
-    # #         st.header("An owl")
-    # #         st.image("https://static.streamlit.io/examples/owl.jpg")
-            
+    with tab2:
+        pass
