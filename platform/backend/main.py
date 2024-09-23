360a02261d6bccf7ecc853fcbff4bafe3cdeaf84
Chonakan Chumtap <cshuhand@gmail.com>
2024-09-23 11:31:20 +0700

    :lipstick: : done main UI.

--- platform/backend/main.py
+++ platform/backend/main.py
@@ -13,7 +13,7 @@ def transform_pipeline_business(df):
     encoder = pickle.load(open("../../pipelines/encoder_business.pkl", 'rb'))
     # encoder = joblib.load('../../pipelines/encoder_business.pkl')
     model = pickle.load(open("../../pipelines/model_LR_business.pkl", 'rb'))
-    scaler = pickle.load(open("../../pipelines/scaler_LR.pkl", 'rb'))
+    scaler = pickle.load(open("../../pipelines/scaler_LR_business.pkl", 'rb'))
     
     all_encoded = ['Inflight wifi service', 
                     'Ease of Online booking', 
@@ -40,6 +40,7 @@ def transform_pipeline_business(df):
     df_encoded_reset = df_encoded.reset_index(drop=True)
 
     df_final = pd.concat([df_encoded_reset, df_drop_dummy_reset], axis=1)
+    print(df_final.shape)
     
     # standardization
     df_scaler = scaler.transform(df_final.iloc[:-6])
@@ -55,10 +56,10 @@ def transform_pipeline_business(df):
 def transform_pipeline_eco(df):
     df = df.drop(["id"], axis=1)
     print(df)
-    encoder = pickle.load(open("../../pipelines/encoder_business.pkl", 'rb'))
+    encoder = pickle.load(open("../../pipelines/encoder_eco.pkl", 'rb'))
     # encoder = joblib.load('../../pipelines/encoder_business.pkl')
-    model = pickle.load(open("../../pipelines/model_LR_business.pkl", 'rb'))
-    scaler = pickle.load(open("../../pipelines/scaler_LR.pkl", 'rb'))
+    model = pickle.load(open("../../pipelines/model_LR_eco.pkl", 'rb'))
+    scaler = pickle.load(open("../../pipelines/scaler_LR_eco.pkl", 'rb'))
     
     all_encoded = ['Inflight wifi service', 
                     'Ease of Online booking', 
@@ -75,6 +76,7 @@ def transform_pipeline_eco(df):
     # # encoding
     df_encoded = df_encoded.astype(int)
     print(df_encoded.shape)
+    
     # print(len(list(df_encoded)))
     # df_encoded = df_encoded[6:]
     df_drop_dummy = df[6:].drop(all_encoded, axis=1)
@@ -85,6 +87,54 @@ def transform_pipeline_eco(df):
     df_encoded_reset = df_encoded.reset_index(drop=True)
 
     df_final = pd.concat([df_encoded_reset, df_drop_dummy_reset], axis=1)
+    df_final.to_csv("eco_encode.csv", index=False)
+    
+    # standardization
+    df_scaler = scaler.transform(df_final.iloc[:-6])
+    preds = model.predict(df_scaler)
+    
+    feature_importance = pd.DataFrame({
+        'Feature': df_final.columns.tolist(),  # List of feature names
+        'Importance': model.coef_[0]  # Coefficients of the logistic regression model
+    }).sort_values(by='Importance', ascending=False)
+    
+    return preds, feature_importance
+
+def transform_pipeline_ecoPlus(df):
+    df = df.drop(["id"], axis=1)
+    print(df)
+    encoder = pickle.load(open("../../pipelines/encoder_ecoPlus.pkl", 'rb'))
+    # encoder = joblib.load('../../pipelines/encoder_business.pkl')
+    model = pickle.load(open("../../pipelines/model_LR_ecoPlus.pkl", 'rb'))
+    scaler = pickle.load(open("../../pipelines/scaler_LR_ecoPlus.pkl", 'rb'))
+    
+    all_encoded = ['Inflight wifi service', 
+                    'Ease of Online booking', 
+                    'Food and drink', 'Online boarding', 
+                    'Seat comfort', 'Inflight entertainment', 
+                    'On-board service', 'Leg room service', 
+                    'Baggage handling', 'Checkin service', 
+                    'Inflight service', 'Cleanliness', 
+                    'Customer Type', 'Type of Travel', 'Class']
+    encoded = encoder.fit_transform(df[all_encoded])
+
+    df_encoded = pd.DataFrame(encoded.toarray(), 
+                            columns=encoder.get_feature_names_out(all_encoded))
+    # # encoding
+    df_encoded = df_encoded.astype(int)
+    print(df_encoded.shape)
+    
+    # print(len(list(df_encoded)))
+    # df_encoded = df_encoded[6:]
+    df_drop_dummy = df[6:].drop(all_encoded, axis=1)
+    
+    df_drop_dummy = df_drop_dummy.drop(["Gender", "Gate location", "Arrival Delay in Minutes", "Departure/Arrival time Convenient"], axis=1)
+    print(df_drop_dummy.shape)
+    df_drop_dummy_reset = df_drop_dummy.reset_index(drop=True)
+    df_encoded_reset = df_encoded.reset_index(drop=True)
+
+    df_final = pd.concat([df_encoded_reset, df_drop_dummy_reset], axis=1)
+    df_final.to_csv("eco_encode.csv", index=False)
     
     # standardization
     df_scaler = scaler.transform(df_final.iloc[:-6])
@@ -158,7 +208,7 @@ def get_business_summary():
     return {"data": data[6:], "preds": preds.tolist(), "feature_importance": feature_importance_records}
 
 @app.get("/eco/summary")
-def get_business_summary():
+def get_eco_summary():
     # Define the scope for Google Sheets API
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 
@@ -173,16 +223,46 @@ def get_business_summary():
     workbook = client.open_by_key(sheet_id)
     
     # Select the first sheet in the workbook
-    sheet = workbook.sheet2
+    sheet = workbook.get_worksheet(1)
     
     # Fetch all records from the sheet
     data = sheet.get_all_records()
     print(data)
     
     df = pd.DataFrame(data)
-    preds, feature_importance = transform_pipeline(df)
-    # Convert to a dictionary (records orientation)
+    preds, feature_importance = transform_pipeline_eco(df)
+    # # Convert to a dictionary (records orientation)
     feature_importance_records = feature_importance.to_dict(orient='records')
 
-    # Return the data as JSON response
+    # # Return the data as JSON response
+    return {"data": data[6:], "preds": preds.tolist(), "feature_importance": feature_importance_records}
+
+@app.get("/ecoPlus/summary")
+def get_eco_summary():
+    # Define the scope for Google Sheets API
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+
+    # Load credentials from the service account JSON file
+    creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+    
+    # Authorize the client using the credentials
+    client = gspread.authorize(creds)
+    
+    # Open the Google Sheet by its sheet ID
+    sheet_id = "1gbTVFkCh1YYlHVN4HpjYP9eFBgAqVneeWKk0qDcKLOE"
+    workbook = client.open_by_key(sheet_id)
+    
+    # Select the first sheet in the workbook
+    sheet = workbook.get_worksheet(2)
+    
+    # Fetch all records from the sheet
+    data = sheet.get_all_records()
+    print(data)
+    
+    df = pd.DataFrame(data)
+    preds, feature_importance = transform_pipeline_ecoPlus(df)
+    # # Convert to a dictionary (records orientation)
+    feature_importance_records = feature_importance.to_dict(orient='records')
+
+    # # Return the data as JSON response
     return {"data": data[6:], "preds": preds.tolist(), "feature_importance": feature_importance_records}
--- platform/frontend/visualizer.py
+++ platform/frontend/visualizer.py
@@ -14,6 +14,11 @@ def get_eco_data():
     response = requests.get(url)
     return response
 
+def get_ecoPlus_data():
+    url = "http://localhost:8000/ecoPlus/summary"
+    response = requests.get(url)
+    return response
+
 if __name__ == "__main__":
     
     st.set_page_config(layout='wide', page_title="AeroPulse Visualization", page_icon="✈️")
@@ -21,34 +26,29 @@ if __name__ == "__main__":
     # Create a container
     with st.container():
         # Create two columns
-        col1, col2, col3 = st.columns([6, 1, 1])  # Adjust the width ratio to create space between the columns
+        col1, col2 = st.columns([6, 1])  # Adjust the width ratio to create space between the columns
         with col1:
             st.title("AeroPulse ✈️")
-            st.markdown("**- Classify Satisfaction, Neutral or Dissatisfaction of passenger based on history data -**")
         with col2:
-            if st.button("Analyze Business Class 💼", type="primary"):
-                res = get_business_data()
-            else:
-                res = None
-        with col3:
-            if st.button("Analyze Economy Class 👜", type="primary"):
-                res = get_eco_data()
-            else:
-                res = None
-                
-        st.markdown(
-            """
-            <style>
-            .stTabs [role="tablist"] {
-                margin-top: 30px; /* Adjust the value to change the margin */
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
+            option = st.selectbox(
+                "Analyzer",
+                ("None", "Economy Class", "Economy Plus Class", "Business Class"),
+            )
+    st.markdown("**- Classify Satisfaction, Neutral or Dissatisfaction of passenger based on history data -**")
+
+    if option == "Economy Class":
+        res = get_eco_data()
+    elif option == "Economy Plus Class":
+        res = get_ecoPlus_data()
+    elif option == "Business Class":
+        res = get_business_data()
+    else:
+        res = None
 
     with st.container(border=True):
-        st.markdown("<h3 style='margin-top: 20px; margin-bottom: 70px; text-align: center;'>US Airplane Flight 123</h3>", unsafe_allow_html=True)
+        if res != None:
+            st.markdown("<h3 style='margin-top: 20px; margin-bottom: 70px; text-align: center;'>CDTI Airplane Flight 640</h3>", unsafe_allow_html=True)
+
         col1, col2 = st.columns(2)
         
         with col1:
@@ -58,7 +58,12 @@ if __name__ == "__main__":
             with st.container():
                 if res != None:
                     st.success('Analyze succesfully', icon="✅")
-                    st.markdown("### Business Class")
+                    if option == "Economy Class":
+                        st.markdown("### :green[Economy Class]")
+                    elif option == "Economy Plus Class":
+                        st.markdown("### :blue[Economy Plus Class]")
+                    elif option == "Business Class":
+                        st.markdown("### :rainbow[Business Class]")
                     df = pd.DataFrame(res.json()['data'])
                     preds = res.json()['preds']
                     feature_importance = res.json()['feature_importance']
@@ -68,10 +73,10 @@ if __name__ == "__main__":
                     count_satisfied = satisfaction_counts['satisfied']
                     count_neutral_dissatisfied = satisfaction_counts['neutral or dissatisfied']
                     
-                    col1, col2 = st.columns(2)
+                    sa_col1, dis_col2 = st.columns(2)
                     
-                    col1.metric(label="Satisfied", value=f"{count_satisfied} 👤", delta="+ 😁")
-                    col2.metric(label="Neutral or Dissatisfied", value=f"{count_neutral_dissatisfied} 👤", delta="- 🫤")
+                    sa_col1.metric(label="Satisfied", value=f"{count_satisfied} 👤", delta="+ 😁")
+                    dis_col2.metric(label="Neutral or Dissatisfied", value=f"{count_neutral_dissatisfied} 👤", delta="- 🫤")
                     
                     # Extracting the features with negative importance from the nested structure
                     negative_features = [item['Feature'] for item in feature_importance if item['Importance'] < 0]
@@ -89,14 +94,14 @@ if __name__ == "__main__":
                     with st.container(border=True):
                         st.markdown("#### Services that need to be developed to enhance passenger satisfaction. 📖")
                         
-                        for name in unique_names:
+                        for name in unique_names[0:3]:
                             st.markdown(f"""
                                         - {name}
                                         """)
                         
                     st.markdown("> based on history data")
         
-        st.markdown("<h3 style='margin-bottom: 20px; text-align: center;'></h3>", unsafe_allow_html=True)
+                    st.markdown("<h3 style='margin-bottom: 20px; text-align: center;'></h3>", unsafe_allow_html=True)
     
     tab1, tab2 = st.tabs(["View Raw Data", "Manual Classify"])
     
